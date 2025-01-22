24383e10b144d1ad3e98cf2e29ebcfb9205cfe43
DangTran-03 <dangvjppro03@gmail.com>
2025-01-22 17:13:00 +0700

    kiem tra so nguyen to

---
+++ kiemTraSoNguyenTo.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+
+    int i, n;
+    bool is_prime = true;
+
+    cout << "Nhap so can kiem tra: ";
+    cin >> n;
+
+    if (n == 0 || n == 1)
+    {
+        is_prime = false;
+    }
+
+    for (i = 2; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            is_prime = false;
+            break;
+        }
+    }
+
+    if (is_prime)
+    {
+        cout << n << " la so nguyen to \n";
+    }
+    else
+    {
+        cout << n << " khong phai la so nguyen to \n";
+    }
+
+    return 0;
+}
